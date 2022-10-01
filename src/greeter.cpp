44ab684c3a869450252e77ed798a8a9756c360f5
raxonpc <tomeczekorlowski1@gmail.com>
2022-10-01 14:35:34 +0200

    Use fmtlib + introduce FetchContent

--- src/greeter.cpp
+++ src/greeter.cpp
@@ -1,6 +1,8 @@
 #include "greeter/greeter.hpp"
+#include <fmt/core.h>
 
 std::string greeter::greetings(const std::string& name)
 {
+    fmt::format("Good morning, {}!", name);
     return { "Good morning, " + name  + "!" };
 }
